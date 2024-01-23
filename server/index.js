b2066815eb80a1a45ea29cd50615b5a138e9eb15
CharlesOsang017 <charlesosango02@gmail.com>
2024-01-23 20:01:34 +0300

    fix environmental file .env

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import dotenv from "dotenv"
 
 
+
+dotenv.config()
 // initialize express app
 const app = express()
+const port = process.env.PORT;
 
-app.listen(3000, (req, res)=>{
-    console.log(`server is running on port 3000`)
+app.listen(port, () =>{
+    console.log(`server is running on  ${port}`)
 })
