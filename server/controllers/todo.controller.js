22400252004f3358e221f3cddb3f74c8cd3e7bdc
CharlesOsang017 <charlesosango02@gmail.com>
2024-01-24 00:49:41 +0300

    Complete update todo API route

--- server/controllers/todo.controller.js
+++ server/controllers/todo.controller.js
@@ -1,6 +1,5 @@
 import Todo from "../models/todo.model.js";
 
-
 // create todo
 export const createTodo = async (req, res) => {
   // const {title, completed} = req.body;
@@ -13,15 +12,33 @@ export const createTodo = async (req, res) => {
 };
 
 // get a todo
-export const getTodo = async(req, res)=>{
-    try {
-        const todos = await Todo.find()
-        if(!todos){
-            res.status(403).json({message: 'No todo available'})
-        }else{
-            res.status(200).json(todos)
-        }
-    } catch (error) {
-        console.log(error.message)
+export const getTodo = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    if (!todos) {
+      res.status(403).json({ message: "No todo available" });
+    } else {
+      res.status(200).json(todos);
     }
-}
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// update a todo
+export const updateTodo = async (req, res) => {
+  const todoId = await Todo.findById(req.params.id);
+  if (!todoId) {
+    res.status(403).json({ message: "todo not found!" });
+  }
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json({ message: "Todo updated successfully!" });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- server/routes/todo.route.js
+++ server/routes/todo.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createTodo, getTodo } from '../controllers/todo.controller.js';
+import { createTodo, getTodo, updateTodo } from '../controllers/todo.controller.js';
 
 const router = express.Router()
 
 router.post('/create', createTodo)
 router.get('/get', getTodo)
+router.post('/update/:id', updateTodo)
 
 export default router;
