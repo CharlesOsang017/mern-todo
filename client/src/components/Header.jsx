9511539474f5fdf0e8999ae25421787cd37bcb41
CharlesOsang017 <charlesosango02@gmail.com>
2024-01-23 12:23:06 +0300

    Initialized tailwindcss and create header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav className='bg-slate-400 w-full flex justify-between h-10'>
+        <div className="my-2 ml-3 cursor-pointer">LOGO <span className="hidden sm:inline cursor-pointer">Todos</span></div>
+        <ul className="my-2 mr-3 flex gap-3">
+            <li className='hover:underline cursor-pointer'>todos</li>
+            <li className='hover:underline cursor-pointer'>about</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Header
